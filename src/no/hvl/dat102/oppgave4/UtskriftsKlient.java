34b5ca10d321d774c8fa28a78a8a0194b122a96a
Ole <Ole@158.37.196.158>
2018-03-20 14:33:21 +0100

    Ole sorting og testet litt i utskriftklient

--- src/no/hvl/dat102/oppgave4/UtskriftsKlient.java
+++ src/no/hvl/dat102/oppgave4/UtskriftsKlient.java
@@ -1,8 +1,24 @@
 package no.hvl.dat102.oppgave4;
 
+import java.util.Random;
+
 public class UtskriftsKlient {
 
 	public static void main(String[] args) {
+
+// Ole test bubble sort	
+		//		Integer[] liste = new Integer[20];
+//		Random r = new Random();
+//		for(int i = 0; i < liste.length; i++) {
+//			Integer temp = new Integer(r.nextInt(100));
+//			liste[i] = temp;
+//			System.out.println(liste[i]);
+//		}
+//		Sorting.bubbleSort(liste);
+//		
+//		for(Integer s : liste) {
+//			System.out.println(s);
+//		}
 		
 		System.out.println("------------------------------------------------------------------");
 		System.out.println("| n\t |antall målinger\t| målt tid\t | teoretisk tid |");
