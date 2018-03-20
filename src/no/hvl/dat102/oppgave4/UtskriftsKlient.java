e51f0b395b023594e7d1830fbbae233b719bc795
Perminow <Perminow@158.37.197.220>
2018-03-20 13:45:28 +0100

    test 2

---
+++ src/no/hvl/dat102/oppgave4/UtskriftsKlient.java
@@ -0,0 +1,23 @@
+package no.hvl.dat102.oppgave4;
+
+public class UtskriftsKlient {
+
+	public static void main(String[] args) {
+		
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("| n\t |antall målinger\t| målt tid\t | teoretisk tid |");
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("|32 000  |\t\t\t|\t\t |\t\t |");
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("|64 000  |\t\t\t|\t\t |\t\t |");
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("|128 000 |\t\t\t|\t\t |\t\t |");
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("|256 000 |\t\t\t|\t\t |\t\t |");
+		System.out.println("------------------------------------------------------------------");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
